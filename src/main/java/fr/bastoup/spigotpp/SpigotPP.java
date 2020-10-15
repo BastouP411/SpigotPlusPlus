92a5c4e2f8c4dcad6e611af9618350f97db2f445
BastouP411 <bastienpm.pascal@gmail.com>
2020-10-15 20:55:52 +0200

    Initial commit

---
+++ src/main/java/fr/bastoup/spigotpp/SpigotPP.java
@@ -0,0 +1,15 @@
+package fr.bastoup.spigotpp;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SpigotPP extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
+}
